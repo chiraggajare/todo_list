d393a23c2905b6d4381ac48e96614f78c28519c7
Chirag Gajare <chiraggajare72@gmail.com>
2025-01-06 20:45:44 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QuickDo</title>
+    <link rel="stylesheet" href="./styles/homestyle.css">
+</head>
+
+<body>
+    <div class="main-section">
+
+        <div class="add-section">
+            <form action="">
+
+                <input type="text" name="title" placeholder="Enter your tasks here">
+                <button type="submit">ADD</button>
+            </form>
+        </div>
+
+
+
+        <div class="show-todo-section">
+            <div class="todo-item">
+                <input type="checkbox">
+                <h2>Sample Task</h2>
+                <br>
+                <small>Created on: --------</small>
+            </div>
+        </div>
+   
+
+        
+    </div>
+</body>
+
+</html>
