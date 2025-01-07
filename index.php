217f4c0462d37c9a2163121004bc5ed4e1952bc2
Chirag Gajare <chiraggajare72@gmail.com>
2025-01-07 12:43:26 +0530

    fixed the blank space after deleting

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require 'db_conn.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>QuickDo</title>
-    <link rel="stylesheet" href="./styles/homestyle.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
